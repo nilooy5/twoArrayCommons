965bd1fed6f0098222a79d6055b431e4141fa4a1
Fazal Mahmud Niloy <niloooy@gmail.com>
2020-08-29 22:49:27 +0600

    [FIX] # for not generating unique numbers

--- src/CommonNumbers.java
+++ src/CommonNumbers.java
@@ -1,17 +1,23 @@
+import java.util.*;
 public class CommonNumbers {
     public static void main(String[] args) {
-        int[] numArray1 = {6, 34, 5, 1, 9};
+        int[] numArray1 = {6, 34, 5, 1, 9, 1};
         int[] numArray2 = {6, 1, 8, 34, 5};
-        int count = 0;
+
+        HashSet<String> unique = new HashSet<>();
+//        int count = 0;
 
         for (int j : numArray1) {
             for (int k : numArray2) {
                 if (j == k) {
-                    count++;
+//                    System.out.println(j);
+                    unique.add(Integer.toString(k));
+//                    count++;
                     break;
                 }
             }
         }
-        System.out.println(count);
+        // System.out.println(count);
+        System.out.println("total unique common integers: " + unique.size());
     }
 }
