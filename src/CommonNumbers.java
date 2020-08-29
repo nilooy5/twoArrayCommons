3cf7761ae6c831bbca4a267bd4e8257b16d62ed5
Fazal Mahmud Niloy <niloooy@gmail.com>
2020-08-29 20:49:13 +0600

    [UPDATE] # output returned following the instruction

--- src/CommonNumbers.java
+++ src/CommonNumbers.java
@@ -1,13 +1,12 @@
 public class CommonNumbers {
     public static void main(String[] args) {
-        int[] numArray1 = {1, 4, 5};
+        int[] numArray1 = {6, 34, 5, 1, 9};
         int[] numArray2 = {6, 1, 8, 34, 5};
         int count = 0;
         // Outer loop
         for (int j : numArray1) {
             for (int k : numArray2) {// inner loop
                 if (j == k) {
-                    System.out.println(j);
                     count++;
                     break;
                 }
